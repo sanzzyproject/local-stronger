9b61a2711db128736474a6147e9fbbb73bb6f3c0
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-13 04:53:07 +0700

    Update LandingHero.jsx

--- src/components/LandingHero.jsx
+++ src/components/LandingHero.jsx
@@ -67,7 +67,7 @@ export default function LandingHero() {
                  <img 
                    src="https://h.top4top.io/p_37230f75i0.jpg" 
                    alt="App Visual"
-                   className="w-32 h-32 object-cover rounded-3xl shadow-lg rotate-3 transform hover:rotate-0 transition-all duration-500"
+                   className="w-40 h-40 object-cover rounded-3xl shadow-lg rotate-3 transform hover:rotate-0 transition-all duration-500"
                  />
                  
               </div>
