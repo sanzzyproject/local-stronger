b81b89ed7cde954e4a483c5cd229ad46f36b889a
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-13 02:45:10 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t border-gray-200 py-8">
+      <div className="max-w-6xl mx-auto px-6 text-center text-gray-600">
+        Developed by <span className="font-semibold text-[#0f172a]">SANN404 FORUM</span>
+      </div>
+    </footer>
+  );
+}
