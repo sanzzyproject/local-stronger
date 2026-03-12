f2c91348d08d96d8321974354e083d20b4c1bb84
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-13 02:37:38 +0700

    Create LandingPage.jsx

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import LandingHero from '../components/LandingHero';
+import FeaturesSection from '../components/FeaturesSection';
+import StatsSection from '../components/StatsSection';
+import PerformanceSection from '../components/PerformanceSection';
+import TrustedTools from '../components/TrustedTools';
+import ContributionSection from '../components/ContributionSection';
+import CtaSection from '../components/CtaSection';
+import Footer from '../components/Footer';
+
+export default function LandingPage() {
+  return (
+    <div className="bg-white text-gray-900 overflow-x-hidden">
+      <LandingHero />
+      <FeaturesSection />
+      <StatsSection />
+      <PerformanceSection />
+      <TrustedTools />
+      <ContributionSection />
+      <CtaSection />
+      <Footer />
+    </div>
+  );
+}
